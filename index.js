849782d452b10dc15d487189ebb18574d06dc032
Soliman-soad <mdsolimansoad@gmail.com>
2022-10-25 09:30:47 +0600

    cors install done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express =require('express');
 const app =express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 const course = require('./data/course.json')
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('welcome to tyro server')
