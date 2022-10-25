004a4863117ed6662b4c7606b9575c33fc0bf6d4
Soliman-soad <mdsolimansoad@gmail.com>
2022-10-25 09:29:00 +0600

    coures page data get done

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express =require('express');
 const app =express();
 const port = process.env.PORT || 5000;
+const course = require('./data/course.json')
 
 app.get('/',(req,res)=>{
     res.send('welcome to tyro server')
+})
+app.get('courses',(req,res)=>{
+    res.send(course)
 })
 
 app.listen(port,()=>{
