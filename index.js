16471eff070142b3b47c7f11f465cf4d4e0dd13e
Soliman-soad <mdsolimansoad@gmail.com>
2022-10-26 16:20:22 +0600

    single course with data link created

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ app.get('/',(req,res)=>{
     res.send('welcome to tyro server');
 })
 app.get('/courses',(req,res)=>{
+    
     res.send(course)
+})
+app.get('/courses/:id', (req,res)=>{
+    const id = req.params.id;
+    const singleCourse = course.find(single => single.id ===1);
+    
+    res.send(singleCourse);
 })
 
 app.listen(port,()=>{
